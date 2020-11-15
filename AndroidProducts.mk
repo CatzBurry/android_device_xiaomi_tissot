6eec3e9cd5e6a1ecf0601b939340aad3864419c4
CatzBurry <myanonymoussad@gmail.com>
2020-11-15 11:57:46 +0700

    tissot: Streakify
    Signed-off-by: CatzBurry <myanonymoussad@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tissot.mk
+    $(LOCAL_DIR)/streak_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tissot-user \
-    derp_tissot-userdebug \
-    derp_tissot-eng \
+    streak_tissot-user \
+    streak_tissot-userdebug \
+    streak_tissot-eng \
